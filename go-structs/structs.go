5c50221a2018b46af0ec18ebdb68dcbd2b2f6fbf
RafaelFRF <rafaelfiorini.rf@gmail.com>
2024-11-08 21:52:36 -0300

    Feat: Implement receiver argument functions

--- go-structs/structs.go
+++ go-structs/structs.go
@@ -12,17 +12,41 @@ type user struct {
 	createdAt time.Time
 }
 
+// Receiver argument | Function attached to the struct
+func (u *user) outputUserDetails() {
+	// Correct way to read
+	// fmt.Println((*u).firstName, (*u).lastName, (*u).birthdate)
+	// Short way Go permit/recomended to use
+	fmt.Println(u.firstName, u.lastName, u.birthdate)
+}
+
+func (u *user) clearUserName() {
+	u.firstName = ""
+	u.lastName = ""
+}
+
+func newUser(firstName, lastName, birthdate string) *user {
+	return &user{
+		firstName: firstName,
+		lastName:  lastName,
+		birthdate: birthdate,
+		createdAt: time.Now(),
+	}
+}
+
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	appUser := user{
-		firstName: userFirstName,
-		lastName:  userLastName,
-		birthdate: userBirthdate,
-		createdAt: time.Now(),
-	}
+	appUser := newUser(userFirstName, userLastName, userBirthdate)
+
+	// appUser := user{
+	// 	firstName: userFirstName,
+	// 	lastName:  userLastName,
+	// 	birthdate: userBirthdate,
+	// 	createdAt: time.Now(),
+	// }
 	// Make sure it's all ordened to make this short way
 	// appUser = user{
 	// 	userFirstName,
@@ -30,14 +54,10 @@ func main() {
 	// 	userBirthdate,
 	// 	time.Now(),
 	// }
-	outputUserDetails(&appUser)
-}
 
-func outputUserDetails(u *user) {
-	// Correct way to read
-	// fmt.Println((*u).firstName, (*u).lastName, (*u).birthdate)
-	// Short way Go permit/recomended to use
-	fmt.Println(u.firstName, u.lastName, u.birthdate)
+	appUser.outputUserDetails()
+	appUser.clearUserName()
+	appUser.outputUserDetails()
 }
 
 func getUserData(promptText string) string {
