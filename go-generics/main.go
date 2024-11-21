7cfb74ebe7dbfe1f9c911a820b7e8039d3d3ac80
RafaelFRF <rafaelfiorini.rf@gmail.com>
2024-11-21 13:06:37 -0300

    Feat: identify how to change the structure for generic and specific types

--- go-generics/main.go
+++ go-generics/main.go
@@ -1,18 +1,25 @@
 package main
 
+import "fmt"
+
 func man() {
 	result := add(1, 2)
+	fmt.Print(result)
 }
 
-func add(a, b interface{}) interface{} {
-	aInt, aIsInt := a.(int)
-	bInt, bIsInt := b.(int)
-	if aIsInt && bIsInt {
-		return aInt + bInt
-	}
-	aFloat, aIsFloat := a.(float64)
-	bFloat, bIsFloat := b.(float64)
-	if aIsFloat && bIsFloat {
-		return aFloat + bFloat
-	}
+// Add generic type for more reusable funcs
+// func add[T any](a, b T) T {
+// Specify the range of types that are allowed
+func add[T int | float64](a, b T) T {
+	// aInt, aIsInt := a.(int)
+	// bInt, bIsInt := b.(int)
+	// if aIsInt && bIsInt {
+	// 	return aInt + bInt
+	// }
+	// aFloat, aIsFloat := a.(float64)
+	// bFloat, bIsFloat := b.(float64)
+	// if aIsFloat && bIsFloat {
+	// 	return aFloat + bFloat
+	// }
+	return a + b
 }
