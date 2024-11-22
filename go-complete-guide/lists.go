2750d7311c62011d9c8f06cb6876336b54d476f2
RafaelFRF <rafaelfiorini.rf@gmail.com>
2024-11-22 16:25:54 -0300

    Feat: basics on slices

--- go-complete-guide/lists.go
+++ go-complete-guide/lists.go
@@ -9,9 +9,16 @@ type Product struct {
 }
 
 func main() {
+	// Array
 	var productNames [4]string = [4]string{"A Book"}
 	prices := [4]float64{10.99, 9.99, 45.99, 20.00}
 	productNames[2] = "A Carpet"
 	fmt.Println(prices[1])
 	fmt.Println(productNames)
+
+	// Slice
+	featurePrices := prices[1:3] // included:excluded -> cold be [1:] or [:3]
+	highlightedPrices := featurePrices[:1]
+	fmt.Println(featurePrices)
+	fmt.Println(highlightedPrices)
 }
