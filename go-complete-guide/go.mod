16ee0462c45e4d2d8b157c41ac648ad4f82e4891
RafaelFRF <rafaelfiorini.rf@gmail.com>
2024-11-22 16:20:34 -0300

    Create: create a new folder and project to work with arrays, slices and maps

---
+++ go-complete-guide/go.mod
@@ -0,0 +1,3 @@
+module example.com/complete-guide
+
+go 1.23.2
---
+++ go-complete-guide/lists.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type Product struct {
+	title string
+	id    string
+	price float64
+}
+
+func main() {
+	var productNames [4]string = [4]string{"A Book"}
+	prices := [4]float64{10.99, 9.99, 45.99, 20.00}
+	productNames[2] = "A Carpet"
+	fmt.Println(prices[1])
+	fmt.Println(productNames)
+}
