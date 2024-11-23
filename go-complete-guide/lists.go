e7e94a6d191c6fac02a237c49c95a907ee95d09a
RafaelFRF <rafaelfiorini.rf@gmail.com>
2024-11-23 16:30:57 -0300

    Feat: deep dive into slices and do an exercise

--- go-complete-guide/lists.go
+++ go-complete-guide/lists.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"example.com/complete-guide/practice"
+)
 
 type Product struct {
 	title string
@@ -9,18 +11,30 @@ type Product struct {
 }
 
 func main() {
-	// Array
-	var productNames [4]string = [4]string{"A Book"}
-	prices := [4]float64{10.99, 9.99, 45.99, 20.00}
-	productNames[2] = "A Carpet"
-	fmt.Println(prices)
-	fmt.Println(prices[1])
-	fmt.Println(productNames)
+	practice.Practice()
+	// prices := []float64{10.99, 8.99}
+	// fmt.Println(prices[1:2])
+	// prices[1] = 9.99
+	// fmt.Println(prices[1:2])
+	// // prices[2] = 10.0 -> Generates error
 
-	// Slice
-	featurePrices := prices[1:3] // included:excluded -> cold be [1:] or [:3]
-	highlightedPrices := featurePrices[:1]
-	fmt.Println(featurePrices)
-	fmt.Println(len(featurePrices), cap(featurePrices))
-	fmt.Println(highlightedPrices)
+	// prices = append(prices, 5.99)
+	// fmt.Println(prices)
+	// prices = prices[1:]
+	// fmt.Println(prices)
 }
+
+// Array
+// var productNames [4]string = [4]string{"A Book"}
+// prices := [4]float64{10.99, 9.99, 45.99, 20.00}
+// productNames[2] = "A Carpet"
+// fmt.Println(prices)
+// fmt.Println(prices[1])
+// fmt.Println(productNames)
+
+//// Slice
+// featurePrices := prices[1:3] // included:excluded -> cold be [1:] or [:3]
+// highlightedPrices := featurePrices[:1]
+// fmt.Println(featurePrices)
+// fmt.Println(len(featurePrices), cap(featurePrices))
+// fmt.Println(highlightedPrices)
---
+++ go-complete-guide/practice/practice.go
@@ -0,0 +1,51 @@
+package practice
+
+import "fmt"
+
+//  7
+type Product struct {
+	title string
+	id    int64
+	price float64
+}
+
+func Practice() {
+	fmt.Println("--------Practice-------")
+	// 1
+	hobbies := [3]string{"Pets", "Games", "Movies"}
+
+	// 2
+	fmt.Println(hobbies[0])
+	fmt.Println(hobbies[1:])
+
+	// 3
+	// hobbiesSliced := hobbies[0:2]
+	hobbiesSliced := hobbies[:2]
+	fmt.Println(hobbiesSliced)
+
+	// 4
+	hobbiesSliced = hobbies[1:3]
+	fmt.Println(hobbiesSliced)
+
+	// 5
+	goalsDynamicList := []string{"Understand Go project's structure", "Learn how to build an Go API"}
+	fmt.Println(goalsDynamicList)
+
+	// 6
+	goalsDynamicList[1] = "Create entire apps by myself"
+	goalsDynamicList = append(goalsDynamicList, "Learn how to build an Go API")
+	fmt.Println(goalsDynamicList)
+
+	// 7
+	productsList := []Product{
+		{title: "dot", id: 1, price: 200.00}, {title: "djTable", id: 140, price: 1200.00},
+	}
+	fmt.Println(productsList)
+	newProduct := Product{
+		title: "laserPen", id: 28, price: 1.00,
+	}
+	productsList = append(productsList, newProduct)
+	fmt.Println(productsList)
+
+	fmt.Println("--------End-Practice-------")
+}
